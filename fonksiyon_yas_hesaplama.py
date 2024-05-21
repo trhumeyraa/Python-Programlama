00216e553d1d6444782bade32689d24bffc60f60
Humeyra Goray <120178302+trhumeyraa@users.noreply.github.com>
2024-05-21 09:03:01 +0300

    Add files via upload

---
+++ fonksiyon_yas_hesaplama.py
@@ -0,0 +1,8 @@
+def yasHesapi (dogumYili):
+               return 2019 - dogumYili
+
+ageCinar = yasHesapi (2017)
+ageAda= yasHesapi (2010)
+
+print (ageCinar, ageAda)
+
---
+++ fonksiyonda_helloworld.py
@@ -0,0 +1,3 @@
+def info():
+ print("hello")
+info() 
